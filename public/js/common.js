22c1d6baee63c56eaa085882ff38cefc9aebd358
Leo Martin Westby <leomarti@stud.ntnu.no>
2014-11-25 12:24:37 +0100

    Moved the polyfill functions to the common.js file

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,24 @@
+//Polyfill functions 
+
+//Returns the current time in ms
+if (!Date.now) {
+	Date.now = function() {
+		return new Date().getTime();
+	}
+}
+
+//Returns the last element of an array
+if (!Array.prototype.last){
+	Array.prototype.last = function() {
+		return this[this.length - 1];
+	};
+};
+
+//Returns a random integer between min and max inclusive
+Math.randomInt = function(min, max) {
+	return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
 var missionTimeUpdater;
 
 //Initializes the missiontime element and starts a timer that decrements it by one every minute until it is 0
