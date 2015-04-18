9155eeffd170b2e917680083edb177263ff15f2f
Carl-Erik Kopseng <carlerik@gmail.com>
2015-04-18 17:28:27 +0200

    Mission commander should also know the time.

--- app/components/commander-app.react.js
+++ app/components/commander-app.react.js
@@ -1,15 +1,19 @@
-var React = require('react');
+const React = require('react');
+const MissionStore = require('../stores/mission-state-store');
+const MissionTimer = require('./mission-timer.react');
 
 
 var App = React.createClass({
 
     render() {
+
+        // put it here due to circular dependencies
         var MissionActionCreators = require('../actions/MissionActionCreators');
 
         return (
             <div>
 
-                <p id="missionTime">Oppdraget har ikke startet</p>
+                { MissionStore.isMissionRunning()? <MissionTimer /> :  <p id="missionTime">Oppdraget har ikke startet</p> }
 
                 <div>
                     <button  onClick={MissionActionCreators.startMission} >Start oppdrag</button>
