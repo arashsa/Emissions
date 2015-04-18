f1db6d6a3777fb0e4c86c8a18c9bba8016e58331
Carl-Erik Kopseng <carlerik@gmail.com>
2015-04-18 16:16:28 +0200

    Removed unneeded node packages

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // transparently require() jsx from node
-// this might not be needed anymore - don't think we transform anything on the server?
+// we don't _need_ it, but as it enables us to continue write ES6 code in our modules,
+// I am keeping it :)
 require('node-jsx').install({harmony: true});
 
 var express = require('express');
@@ -8,28 +9,26 @@ var port = process.env.PORT || 5000;
 var server = app.listen(port);
 var socketIo = require('socket.io');
 var io = socketIo.listen(server);
-var bootstrapScript = require('./server/server-bootstrap');
 var htmlCssDir;
-
-
 console.log("Server listening on port " + port);
 
-// enable gzip compression
-var compression = require('compression')
-app.use(compression());
-
 if ('production' === process.env.NODE_ENV) {
+    console.log('Running a production build');
     htmlCssDir = __dirname + '/dist';
 } else {
+    console.log('Running a development build');
     htmlCssDir = __dirname + '/build';
 }
 console.log('Serving html files from ', htmlCssDir);
 
-// set up routing
+// enable gzip compression
+app.use(require('compression')());
 
+// set up routing
 app.use(express.static(htmlCssDir));
 app.use(express.static(__dirname + '/assets'));
 
+// enable us to check the environment by going to /environment
 app.get('/environment', function (req, res) {
     res.json(process.env);
 });
@@ -45,7 +44,6 @@ app.get('*', function (req, res) {
 // set up server api using Socket.IO
 require('./server/server-api')(io);
 
-
 // convenience stuff to run at startup when developing
 // run startup events - just when developing
-bootstrapScript.run();
+require('./server/server-bootstrap').run();
